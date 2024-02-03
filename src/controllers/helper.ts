f2dc5d3b59200751f87ac23089cd0ea5dc075099
ElitonSouza <ess3@icomp.ufam.edu.br>
2024-02-02 21:27:24 -0400

    distribuindo melhor os helpers

--- src/controllers/helper.ts
+++ src/controllers/helper.ts
@@ -1,4 +1,4 @@
-export const cadastrarUsuario = (error: any ) => {
+export const tratamentosErros = (error: any ) => {
 
     if (error.name === 'SequelizeUniqueConstraintError') {
       const str = error.errors[0].value;
@@ -7,8 +7,15 @@ export const cadastrarUsuario = (error: any ) => {
       return ({ error: novaStr + ' já está cadastrado(a) no sistema' });
     }
 
-
-
-    return ({error: error});
-  
+    return ({error: error});  
 };
+
+export const formataNumero = (numero: string) => {
+  // Remove o '9' da terceira posição
+  const numeroSemNove = numero.slice(0, 2) + numero.slice(3);
+
+  // Adiciona os 3 primeiros caracteres (+55)
+  const numeroComCodigoPais = `+55${numeroSemNove}`;
+
+  return numeroComCodigoPais;
+}
