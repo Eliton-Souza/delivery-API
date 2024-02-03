68bd947564389e4a6a025c4c593f5a7d7bf27824
ElitonSouza <ess3@icomp.ufam.edu.br>
2024-02-02 21:30:41 -0400

    deixando nome de variaveis para mais intuitivas

--- src/controllers/verificacaoController.ts
+++ src/controllers/verificacaoController.ts
@@ -1,21 +1,23 @@
 import { Request, Response } from 'express';
-import { deletarLoginsExpirados, registraCodigo, verificaCode, verificaNumero } from '../services/serviceVerificacao';
+import { deletarLoginsExpirados, registraCodigo, verificaCelular, verificaCode } from '../services/serviceVerificacao';
 import { enviarMSG } from '../services/serviceEnviarMsg';
+import { formataNumero } from './helper';
 
 
 //faz o envio de um novo token ou manda aguardar
-export const validaNumero = async (req: Request, res: Response) => {
+export const validaCelular = async (req: Request, res: Response) => {
 
-  const { numero } = req.body;
+  const { celular } = req.body;
+  const numeroFormatado = formataNumero(celular);
 
   try {
-    await verificaNumero(numero);
-    const registro = await registraCodigo(numero);
-    await enviarMSG(numero, registro.codigo);
+    await verificaCelular(celular);                         // Verifica se um número de telefone já está registrado na tabela temporária ou token expirado
+    const registro = await registraCodigo(celular);        //registra um novo codigo na tabela temporaria
+    await enviarMSG(numeroFormatado, registro.codigo);    //envia mensagem de validação
 
     return res.status(200).json({ success: true, message: 'Mensagem enviada com sucesso' });
   } catch (error: any) {
-    return res.status(500).json({ success: false, error: error.message });
+    return res.json({ success: false, error: error.message });
   }
 };
 
@@ -25,15 +27,15 @@ export const validaNumero = async (req: Request, res: Response) => {
 //faz a validação do codigo q o usuario forneceu
 export const validaCodigo = async (req: Request, res: Response) => {
 
-  const { numero, codigo } = req.body;
+  const { celular, codigo } = req.body;
 
   try {
    
-    await verificaCode(numero, codigo);
+    await verificaCode(celular, codigo);
   
-    return res.status(200).json({ success: true, message: 'Codigo validado com sucesso' });
+    return res.status(200).json({ success: true, message: 'Código validado com sucesso!' });
   } catch (error: any) {
-    return res.status(500).json({ success: false, error: error.message });
+    return res.json({ success: false, error: error.message });
   }
 };
 
